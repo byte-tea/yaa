b73957edf88b8c85c56b5a492b8ab86db1d45327
PJ568 <PJ568@PJ568.eu.org>
2025-03-30 21:38:53 +0800

    【格式】继承 BaseAPI 类并优化代码格式
    - 让 OpenAI_API 类继承自 BaseAPI 类
    - 优化代码格式，如空格和注释
    - 将 API URL 解析相关的代码调整为更清晰的格式

--- yaa/LLM_API/OpenAI.py
+++ yaa/LLM_API/OpenAI.py
@@ -2,7 +2,9 @@ import http.client
 import json
 from urllib.parse import urlparse
 
-class OpenAI_API:
+from ..LLM_API import BaseAPI
+
+class OpenAI_API(BaseAPI):
     def request(session_data):
         # 获取配置
         provider_config = session_data['config']['llm_api']['provider']
@@ -10,7 +12,7 @@ class OpenAI_API:
         api_key = provider_config['api_key']
         model_name = provider_config['model_name']
 
-        # 解析API URL
+        # 解析 API URL
         parsed_url = urlparse(api_url)
         host = parsed_url.netloc
         path = parsed_url.path
@@ -34,7 +36,7 @@ class OpenAI_API:
         res = conn.getresponse()
         data = json.loads(res.read().decode("utf-8"))
         
-        # 添加AI回复到消息列表
+        # 添加 AI 回复到消息列表
         ai_response = data['choices'][0]['message']
         session_data['messages'].append(ai_response)
         
